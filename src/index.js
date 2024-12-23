ba3eb0467180f76b33ca6f52b59775d55654b13b
Romte1 <ivan_ni224@hotmail.com>
2024-12-23 10:02:40 -0300

    Added a toggleable nav bar as a practice

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./styles.css";
+let toggleButton = document.querySelector(".toggleNavBarButton");
+let navMenu = document.querySelector(".navMenu");
+let body = document.querySelector("body");
+
+function toggleNavBar() {
+  if (toggleButton.textContent === "collapse") {
+    toggleButton.textContent = "show";
+    navMenu.style.display = "none";
+    body.style.gridTemplateColumns = "max-content minmax(300px, auto)";
+  } else {
+    toggleButton.textContent = "collapse";
+    navMenu.style.display = "block";
+    body.style.gridTemplateColumns = "minmax(100px, 300px) minmax(300px, auto)";
+  }
+}
+toggleButton.addEventListener("click", () => {
+  toggleNavBar();
+});
